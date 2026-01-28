46d2e2cbafffa1e8a6790f6c968630ff3e0eb3c0
muneeskumar007 <muneeskumar0000@gmail.com>
2026-01-28 20:10:37 +0530

    Fix Netlify SPA routing

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -255,7 +255,7 @@ const saveStudentIfNotExists = async (user) => {
           //   />
           //   Continue with Google
           // </button>
-          <div className="relative mt-12">
+          <div className="relative mt-9">
   {/* 🔒 Coming Soon Tag */}
   <div className="absolute -top-3 left-1/2 z-10 -translate-x-1/2 rounded-full bg-gray-800 px-3 py-1 text-xs font-semibold text-white shadow-lg">
     🔒 Coming Soon
