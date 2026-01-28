d49feae66765f6031b12fd428962d2b6c315baf6
muneeskumar007 <muneeskumar0000@gmail.com>
2026-01-28 19:07:10 +0530

    Fix Netlify SPA routing

--- src/firebase.js
+++ src/firebase.js
@@ -11,15 +11,26 @@ import { getAuth } from 'firebase/auth';
 import { getFunctions } from 'firebase/functions';
 
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBWNk-GTq2OXRN95-gYehMP2OQfpz6dFlo",
+//   authDomain: "quize-7f983.firebaseapp.com",
+//   projectId: "quize-7f983",
+//   storageBucket: "quize-7f983.firebasestorage.appspot.com",
+//   messagingSenderId: "1089648503737",
+//   appId: "1:1089648503737:web:6f931807ce55f8192b2468",
+//   measurementId: "G-9M57FGC3H8"
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBWNk-GTq2OXRN95-gYehMP2OQfpz6dFlo",
-  authDomain: "quize-7f983.firebaseapp.com",
-  projectId: "quize-7f983",
-  storageBucket: "quize-7f983.firebasestorage.appspot.com",
-  messagingSenderId: "1089648503737",
-  appId: "1:1089648503737:web:6f931807ce55f8192b2468",
-  measurementId: "G-9M57FGC3H8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
  const app = initializeApp(firebaseConfig);
